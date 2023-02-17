707d7a20d60b2d91ad869bbc8f7f6f40d4b45a8e
Kenan <K.Karalioglu@maerz-network.de>
2023-02-17 10:17:32 +0100

    Rework layout

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,36 @@
 <script setup>
+    import InputText from 'primevue/inputtext';
+    import Divider from 'primevue/divider';
+    import Button from 'primevue/button';
+
+    import Uploader from './Uploader.vue';
+
+    const uploads = [
+        { title: 'PwnDoc - Json', info: 'Json from PwnDoc' },
+        { title: 'Scanned - Domains', info: 'CSV of scanned domains' }, 
+        { title: 'Excluded - Domains', info: 'CSV of excluded domains' },
+    ]
 </script>
 
 <template>
-    <div class='w-screen h-screen flex justify-content-center align-items-center'>
-        <h1>App</h1>
+    <div class='w-screen h-screen flex justify-content-center align-items-center surface-100'>
+        <div class='container surface-50 gap-2'>
+            <div class='w-full flex align-items-center p-3'>
+                <h2 class='font-bold'>Create Report</h2>
+            </div>
+            <div class='w-full flex justify-content-between align-items-center gap-2 p-3'>
+                <InputText class='w-full' placeholder='Title*' />
+                <Button class='p-button-info p-button-raised w-12rem' label='Create' />
+            </div>
+            <h2 class='font-bold mx-3'>Uploads</h2>
+            <div class='w-full px-3'>
+                <Divider />
+            </div>
+            <Uploader v-for='(upload, i) in uploads'
+                :key='i'
+                :title='upload.title'
+                :info='upload.info'
+            />
+        </div>
     </div>
 </template>
---
+++ src/components/Uploader.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import Button from 'primevue/button';
+</script>
+
+<template>
+    <div class='w-full p-3'>
+        <div class='flex align-items-center justify-content-between'>
+            <div class='flex align-items-center gap-3'>
+                <i class='text-500 pi pi-upload' />
+                <div>
+                    <h4 class='font-semibold'>{{ $attrs.title }}</h4>
+                    <p class='font-light text-sm'>{{ $attrs.info }}</p>
+                </div>
+            </div>
+            <Button class='p-button-info p-button-rounded p-button-raised' icon='pi pi-plus'  />
+        </div>
+    </div>
+</template>
