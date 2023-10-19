5049926ea85589d5f513299aba4c1e35ddc27448
leo <l.huth@maerz-network.de>
2023-10-19 17:34:38 +0200

    code cleanup

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,199 +1,208 @@
 <script setup>
-    import ProgressSpinner from 'primevue/progressspinner';
-    import InputText from 'primevue/inputtext';
-    import Dropdown from 'primevue/dropdown';
-    import Divider from 'primevue/divider';
-    import Button from 'primevue/button';
-    
-    import Uploader from './Uploader.vue';
+import ProgressSpinner from 'primevue/progressspinner';
+import InputText from 'primevue/inputtext';
+import Dropdown from 'primevue/dropdown';
+import Divider from 'primevue/divider';
+import Button from 'primevue/button';
 
-    import {ref, inject, onMounted, computed} from 'vue';
+import Uploader from './Uploader.vue';
 
-    import {
-      handle_ansi,
-      handle_image,
-      handle_text,
-      parse_csv
-    } from '../utils';
+import {ref, inject, onMounted, computed} from 'vue';
 
-    const loading = ref(false);
+import {
+  handle_ansi,
+  handle_image,
+  handle_text,
+  parse_csv
+} from '../utils';
 
-    const data = ref({
-        propability: '',
-        environment: '',
-        pwndoc_json: '',
-        excluded: '',
-        security: '',
-        scanned: '',
-        damage: '',
-        title: '',
-    });
+const loading = ref(false);
 
-    const form_valid = computed(()=>{
-      try{
-        Object.entries(data.value).forEach((entry)=>{
-          console.log('scanning...', entry)
-          if(entry[1]==""){
-            console.log('exept')
-            throw new Error('form invalid')
-          }
-        })
-      } catch (e){
-        console.log('whooops')
-        return false
+const data = ref({
+  propability: '',
+  environment: '',
+  pwndoc_json: '',
+  excluded: '',
+  security: '',
+  scanned: '',
+  damage: '',
+  title: '',
+});
+
+const form_valid = computed(() => {
+  try {
+    Object.entries(data.value).forEach((entry) => {
+      console.log('scanning...', entry)
+      if (entry[1] == "") {
+        console.log('exept')
+        throw new Error('form invalid')
       }
-      return true
-
     })
+  } catch (e) {
+    console.log('whooops')
+    return false
+  }
+  return true
 
-    const up = ref(null)
+})
 
-    const uploads = [
-        { title: 'PwnDoc - Json', info: 'Json from PwnDoc',file_tpe:'.json', callback: async file => data.value.pwndoc_json = await handle_text(file) },
-        { title: 'Scanned - Domains', info: 'CSV of scanned scopes',file_tpe:'.csv', callback: async file => data.value.scanned = await handle_ansi(file) },
-        { title: 'Excluded - Domains', info: 'CSV of excluded scopes',file_tpe:'.csv', callback: async file => data.value.excluded = await handle_ansi(file) },
-        { title: 'Security - Projects', info: 'CSV of security projects',file_tpe:'.csv', callback: async file => data.value.security = await handle_ansi(file) },
-        { title: 'Scan - Environment', info: '.jpg, .jpeg, .png',file_tpe:'.png,.jpeg,.jpg', callback: async file => data.value.environment = await handle_image(file) },
-    ]
+const up = ref(null)
 
-    const propabilities = [
-        { name: 'Sehr Wahrscheinlich' },
-        { name: 'Wahrscheinlich' },
-        { name: 'Möglich' },
-        { name: 'Unwahrscheinlich' },
-        { name: 'Nahezu Unmöglich' },
-    ];
+const uploads = [
+  {
+    title: 'PwnDoc - Json',
+    info: 'Json from PwnDoc',
+    file_tpe: '.json',
+    callback: async file => data.value.pwndoc_json = await handle_text(file)
+  },
+  {
+    title: 'Scanned - Domains',
+    info: 'CSV of scanned scopes',
+    file_tpe: '.csv',
+    callback: async file => data.value.scanned = await handle_ansi(file)
+  },
+  {
+    title: 'Excluded - Domains',
+    info: 'CSV of excluded scopes',
+    file_tpe: '.csv',
+    callback: async file => data.value.excluded = await handle_ansi(file)
+  },
+  {
+    title: 'Security - Projects',
+    info: 'CSV of security projects',
+    file_tpe: '.csv',
+    callback: async file => data.value.security = await handle_ansi(file)
+  },
+  {
+    title: 'Scan - Environment',
+    info: '.jpg, .jpeg, .png',
+    file_tpe: '.png,.jpeg,.jpg',
+    callback: async file => data.value.environment = await handle_image(file)
+  },
+]
 
-    const damages = [
-        { name: 'Niedrig' },
-        { name: 'Mittel' },
-        { name: 'Hoch' },
-        { name: 'Sehr Hoch' },
-        { name: 'Kritisch' },
-    ]
+const propabilities = [
+  {name: 'Sehr Wahrscheinlich'},
+  {name: 'Wahrscheinlich'},
+  {name: 'Möglich'},
+  {name: 'Unwahrscheinlich'},
+  {name: 'Nahezu Unmöglich'},
+];
 
-    const add_message = inject('add_message');
+const damages = [
+  {name: 'Niedrig'},
+  {name: 'Mittel'},
+  {name: 'Hoch'},
+  {name: 'Sehr Hoch'},
+  {name: 'Kritisch'},
+]
 
-    function handle_create() {
-        let entity_index = 0;
+const add_message = inject('add_message');
 
-        const entity_map = {
-            'Titel': data.value.title,
-            'Wahrscheinlichkeit': data.value.propability.name,
-            'Schadensausmass': data.value.damage.name,
-            'PwnDoc - Json': data.value.pwndoc_json,
-            'Scanned - Domains': data.value.scanned,
-            'Excluded - Domains': data.value.excluded,
-            'Security - Projects': data.value.security,
-            'Scan - Environment': data.value.environment
-        };
-/*
-        Object.keys(entity_map).forEach(key => {
-            if (!entity_map[key])
-                setTimeout(() => add_message({
-                    summary: 'Incorrect input',
-                    detail: `${key} not defined`,
-                    severity: 'info',
-                    life: 3000
-                }), entity_index++ * 400);
-        });*/
+function handle_create() {
+  let entity_index = 0;
 
-        if (entity_index <= 0) {
-            const json = JSON.parse(data.value.pwndoc_json);
+  const entity_map = {
+    'Titel': data.value.title,
+    'Wahrscheinlichkeit': data.value.propability.name,
+    'Schadensausmass': data.value.damage.name,
+    'PwnDoc - Json': data.value.pwndoc_json,
+    'Scanned - Domains': data.value.scanned,
+    'Excluded - Domains': data.value.excluded,
+    'Security - Projects': data.value.security,
+    'Scan - Environment': data.value.environment
+  };
+  /*
+          Object.keys(entity_map).forEach(key => {
+              if (!entity_map[key])
+                  setTimeout(() => add_message({
+                      summary: 'Incorrect input',
+                      detail: `${key} not defined`,
+                      severity: 'info',
+                      life: 3000
+                  }), entity_index++ * 400);
+          });*/
 
-            json['excludedDestinations'] = parse_csv(data.value.excluded);
-            json['securityProjects'] = parse_csv(data.value.security);
-            json['scanDestinations'] = parse_csv(data.value.scanned);
+  if (entity_index <= 0) {
+    const json = JSON.parse(data.value.pwndoc_json);
 
-            json['customerScanEnv'] = data.value.environment;
-            json['reportTitle'] = data.value.title;
+    json['excludedDestinations'] = parse_csv(data.value.excluded);
+    json['securityProjects'] = parse_csv(data.value.security);
+    json['scanDestinations'] = parse_csv(data.value.scanned);
 
-            json['riskMatrix'] = `
+    json['customerScanEnv'] = data.value.environment;
+    json['reportTitle'] = data.value.title;
+
+    json['riskMatrix'] = `
                 ${data.value.propability.replace(' ', '')}
                 ${data.value.damage.replace(' ', '')}
             `.replace(/(\r\n|\n|\r| )/gm, '');
 
-            fetch('http://127.0.0.1:5000/report/api/generate', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(json)
-            }).then(response => {
-                loading.value = false;
+    fetch('http://127.0.0.1:5000/report/api/generate', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(json)
+    }).then(response => {
+      loading.value = false;
 
-                if (response.status == '200')
-                    window.open(response.url)
-            });
+      if (response.status == '200')
+        window.open(response.url)
+    });
 
-            loading.value = true;
-        }
-    }
+    loading.value = true;
+  }
+}
 </script>
 
 <template>
-    <div class='w-screen h-screen flex justify-content-center align-items-center surface-100'>
-        <div class='container border-round-lg surface-50 gap-1 p-3'>
-<!--            <h2 class='font-bold'>Create Report</h2>-->
-            <div >
-                <InputText v-model='data.title' class='w-full shadow-none' required placeholder='Title' />
-<!--                <div>
-                    <ProgressSpinner v-if='loading' class='w-4rem'  />
-                    <Button v-else class='p-button-info p-button-raised w-12rem' label='Create' @click='handle_create' />
-                </div>-->
-            </div>
-<!--            <div class='w-full px-3'>
-                <Divider />
-            </div>-->
-          <h2 class='font-bold mt-5'>Risiko Matrix</h2>
-            <div class='grid mt-0 align-items-center'>
-<!--                <Dropdown v-model='data.propability' :options='propabilities' optionLabel='name' class='w-full' placeholder='Wahrscheinlichkeit' />
-                <Dropdown v-model='data.damage' :options='damages' optionLabel='name' class='w-full' placeholder='Schadensausmass' />-->
-              <ListBox class="col surface-50" v-model="data.propability" option-value="name" :options="propabilities" option-label="name"/>
-              <Divider class="mx-0 align-self-stretch" layout="vertical" />
-              <ListBox class="col surface-50 " v-model='data.damage' option-value="name" :options="damages" option-label="name"/>
-            </div>
-            <h2 class='font-bold mt-4' style="margin-bottom:12px">Uploads</h2>
-            <div>
-              <Uploader style="margin-bottom:8px" @callback="uploads[0].callback" :title="uploads[0].title" :info="uploads[0].info" :file_type="uploads[0].file_tpe"/>
-              <div style="display:grid; grid-template-columns:1fr 1fr; gap:0.5rem">
-                <Uploader v-for='(upload, i) in uploads.slice(1)'
-                          :key='i'
-                          @callback='upload.callback'
-                          :title='upload.title'
-                          :info='upload.info'
-                          :file_type="upload.file_tpe"
-                />
-              </div>
-
-<!--              <FileUpload
-                  ref="up"
-                  name="destinations"
-                  mode="advanced"
-                  accept=".json, .csv, .png"
-                  file-limit="5"
-                  :preview-width="0"
-                  :show-upload-button="false"
-                  :show-cancel-button="false"
-                  multiple
-              />-->
-            </div>
-          <Button class="mt-5" :disabled="!form_valid" label="Create Report" severity="secondary" @click='handle_create' />
+  <div class='w-screen h-screen flex justify-content-center align-items-center surface-100'>
+    <div class='container border-round-lg surface-50 gap-1 p-3'>
+      <div>
+        <InputText v-model='data.title' class='w-full shadow-none' placeholder='Title' required/>
+      </div>
+      <h2 class='font-bold mt-4' style="padding-top:4px">Risiko Matrix</h2>
+      <div class='grid mt-0 align-items-center'>
+        <!--                <Dropdown v-model='data.propability' :options='propabilities' optionLabel='name' class='w-full' placeholder='Wahrscheinlichkeit' />
+                        <Dropdown v-model='data.damage' :options='damages' optionLabel='name' class='w-full' placeholder='Schadensausmass' />-->
+        <ListBox v-model="data.propability" :options="propabilities" class="col surface-50" option-label="name"
+                 option-value="name"/>
+        <Divider class="mx-0 align-self-stretch" layout="vertical"/>
+        <ListBox v-model='data.damage' :options="damages" class="col surface-50 " option-label="name"
+                 option-value="name"/>
+      </div>
+      <h2 class='font-bold mt-4' style="margin-bottom:12px">Uploads</h2>
+      <div>
+        <Uploader :file_type="uploads[0].file_tpe" :info="uploads[0].info" :title="uploads[0].title"
+                  style="margin-bottom:8px" @callback="uploads[0].callback"/>
+        <div style="display:grid; grid-template-columns:1fr 1fr; gap:0.5rem">
+          <Uploader v-for='(upload, i) in uploads.slice(1)'
+                    :key='i'
+                    :file_type="upload.file_tpe"
+                    :info='upload.info'
+                    :title='upload.title'
+                    @callback='upload.callback'
+          />
         </div>
+
+      </div>
+      <Button :disabled="!form_valid" class="mt-5" label="Create Report" severity="secondary" @click='handle_create'/>
     </div>
+  </div>
 </template>
 
 <style scoped>
-:deep(.p-fileupload-file-thumbnail){
+:deep(.p-fileupload-file-thumbnail) {
   visibility: hidden;
 }
 
-:deep(.p-listbox-list) li{
+:deep(.p-listbox-list) li {
   text-align: center;
   border-radius: 0.375rem !important;
 }
 
 :deep(.p-listbox) {
-  border:none;
-  box-shadow:none;
+  border: none;
+  box-shadow: none;
   padding-bottom: 0;
 }
 </style>
